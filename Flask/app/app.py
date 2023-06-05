04ced5d3e7e4694140a8f0fe6adb1c58b0aabcdc
Tomás Macrade <tomasmacrade@gmail.com>
2023-06-05 20:48:52 -0300

    Parte I lista

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -29,12 +29,23 @@ if __name__ == '__main__':
         Citas_Sem = Aux["Veces citado"]
         Cand_Tw = Data["Candidato"].value_counts().index.tolist()[0]
         Cand_Tw_Num = Data["Candidato"].value_counts().values.tolist()[0]
+        Esp_Tw = Data["Espacio"].value_counts().index.tolist()[0]
+        Esp_Tw_Num = Data["Espacio"].value_counts().values.tolist()[0]
+        Cand_Rel = Data.groupby("Candidato").mean()["Relevancia"].sort_values(ascending=False).index.tolist()[0]
+        Cand_Rel_Num = Data.groupby("Candidato").mean()["Relevancia"].sort_values(ascending=False).values.tolist()[0]
+        Esp_Rel = Data.groupby("Espacio").mean()["Relevancia"].sort_values(ascending=False).index.tolist()[0]
+        Esp_Rel_Num = Data.groupby("Espacio").mean()["Relevancia"].sort_values(ascending=False).values.tolist()[0]
+
+
 
         return render_template('Home.html',Texto_semana=Texto_semana,Nombre_semana=Nombre_semana,Usr=Usr_Sem,
                                Url=Url_Sem,Fecha_Sem=Fecha_Sem,Likes_Sem=Likes_Sem,
                                Rt_Sem=Rt_Sem,
                                Citas_Sem=Citas_Sem,
-                               Cand_Tw=Cand_Tw,Cand_Tw_Num=Cand_Tw_Num)
+                               Cand_Tw=Cand_Tw,Cand_Tw_Num=Cand_Tw_Num,
+                               Esp_Tw=Esp_Tw,Esp_Tw_Num=Esp_Tw_Num,
+                               Cand_Rel=Cand_Rel,Cand_Rel_Num=round(Cand_Rel_Num,1),
+                               Esp_Rel=Esp_Rel,Esp_Rel_Num=round(Esp_Rel_Num,1))
 
 
     # Ruta para consultar : http://localhost:5000//results?Nombrecandidato
@@ -47,6 +58,6 @@ if __name__ == '__main__':
 
         return "Acá van a aparecer los resultados del candidato {}".format(list(query_values.keys())[0])
 
-    app.run(debug=False, host='0.0.0.0', port=5000) # Ejecución del servidor/API
+    app.run(debug=True, host='0.0.0.0', port=5000) # Ejecución del servidor/API
 
 
